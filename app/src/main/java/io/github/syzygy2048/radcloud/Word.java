0fa3c95fed476b135eb74ced6d7a1cd054c2c4ac
Rebeka Koszticsák <rebebaba2@gmail.com>
2018-05-20 13:30:14 +0200

    not working hashcode

---
+++ app/src/main/java/io/github/syzygy2048/radcloud/Word.java
@@ -0,0 +1,67 @@
+package io.github.syzygy2048.radcloud;
+
+import android.support.annotation.NonNull;
+
+import java.util.Objects;
+
+/**
+ * Created by rebeb on 5/8/2018.
+ */
+
+public class Word {
+
+
+    private String term;
+    private float tf = -1.0f;
+    private float itf = -1.0f;
+
+    public Word(String term) {
+        this.term = term;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public float getTf() {
+        return tf;
+    }
+
+    public float getItf() {
+        return itf;
+    }
+
+
+    public void setTf(float tf) {
+        this.tf = tf;
+    }
+
+    public void setItf(float itf) {
+        this.itf = itf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Word)) {
+            throw new ClassCastException();
+        }
+        Word w = (Word) o;
+        if (w.getTerm().equals(term)) {
+            System.out.println("They are equal");
+            return true;
+        } else {
+            System.out.println("They are not");
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        System.out.println("Term: " + term + ", hash: " + term.hashCode());
+        return term.hashCode();
+//        int result = 17;
+//        result = 31 * result + term.hashCode();
+
+//        return result;
+    }
+}
